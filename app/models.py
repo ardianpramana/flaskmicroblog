55188ca61a33d2017b48fcd3da8a2ad285eb54f1
Ardian Pramana <pramana.ardian@gmail.com>
2018-06-12 15:17:02 +0700

    [add] Part V: add pasword hash.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,9 +10,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-
     def __repr__(self):
         return '<user {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -18,7 +25,6 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
     def __repr__(self):
         return '<post {}>'.format(self.body)
